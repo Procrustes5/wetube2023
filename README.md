66e29d786bb1bc6a302e1beb8f057236d053cab4
Hanse Kim <procrustes5@gmail.com>
2023-03-17 23:12:42 +0900

    230317 Routers and Controllers

---
+++ README.md
@@ -0,0 +1,16 @@
+# WeTube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log out
+/users/edit -> Edit User
+/users/remove -> Remove User
+
+/videos/:id -> Watch Video
+/videos/:id/edit -> Edit Video
+/videos/:id/remove -> Remove Video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  console.log("Somebody is trying to go home.");
-  return res.send("hello");
-};
-
 app.use(logger);
-app.get("/", handleHome);
 
-const handleListening = () =>
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
+};
 
 app.listen(PORT, handleListening);
