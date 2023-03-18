d1378bae43d50ee6bcc54415129910ceea5f49a9
Hanse Kim <procrustes5@gmail.com>
2023-03-18 22:08:06 +0900

    230318 pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const see = (req, res) => res.send("See Video");
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit Video");
-export const trending = (req, res) => res.send("Home");
+export const trending = (req, res) => res.render("Home");
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { edit, see, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/see", see);
 videoRouter.get("/upload", upload);
-videoRouter.get(":id/edit", edit);
-videoRouter.get(":id/delete", deleteVideo);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
