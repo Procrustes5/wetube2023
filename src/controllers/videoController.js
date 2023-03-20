2f880b5085aac88f2491afd3d8cde1a4f79c5d71
Hanse Kim <procrustes5@gmail.com>
2023-03-20 23:34:06 +0900

    230320 Pug Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,7 @@
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.send("Edit Video");
-export const trending = (req, res) => res.render("Home");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) =>
+  res.render("Edit Video", { pageTitle: "Edit Video" });
+export const trending = (req, res) => res.render("Home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
