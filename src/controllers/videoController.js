3cc470d0f3edc8a8655c2cf94bce9395544d16ef
Hanse Kim <procrustes5@gmail.com>
2023-03-28 22:46:02 +0900

    230328 C of CRUD

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,6 @@ export const home = async (req, res) => {
 
 export const watch = (req, res) => {
   const { id } = req.params;
-
   return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
@@ -22,12 +21,25 @@ export const getEdit = (req, res) => {
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
+  const { title } = req.body;
   return res.redirect(`/videos`);
 };
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
