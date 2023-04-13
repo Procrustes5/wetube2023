f27a06b810c5c0bffc88a294b785f9f732af897c
Hanse Kim <procrustes5@gmail.com>
2023-04-13 22:55:24 +0900

    230413 Logout / Edit profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -43,7 +43,7 @@ export const getLogin = (req, res) => {
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -114,13 +114,10 @@ export const finishGithubLogin = async (req, res) => {
     if (!emailObj) {
       return res.redirect("/login");
     }
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatar_url,
         name: userData.name ? userData.name : "Unknown",
         email: emailObj.email,
         username: userData.login,
@@ -128,8 +125,9 @@ export const finishGithubLogin = async (req, res) => {
         socialOnly: true,
         location: userData.location ? userData.location : "Unknown",
       });
+    } else {
       req.session.loggedIn = true;
-      req.session.user = existingUser;
+      req.session.user = user;
       return res.redirect("/");
     }
   } else {
@@ -137,7 +135,14 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Delete User");
-export const logout = (req, res) => res.send("Logout");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
